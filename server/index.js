c09bb7b55da2686d2319b5f9a988fa52c70a362f
skm-049 <one.001skm@gmail.com>
2023-12-29 13:30:26 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 // index.js
 const express = require('express')
+const mongoose = require("mongoose")
+const cors = require("cors")
+require("dotenv").config()
+
 const app = express()
 const PORT = 80
+
+/* CONNECTING TO MONGODB */
+const URI = process.env.atlasDBUrl
+
+const connectDB =async () => {
+    try{
+        await mongoose.connect(URI, {});
+        console.log("MongoDB Connected...");
+}
+    catch(error) {
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+app.use(cors({credentials:this.true,}));
 
 
 app.get('/', (req, res) => {
