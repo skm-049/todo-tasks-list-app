471af554008e71f0e023554f74a9ca73ebf10126
Somesh Kumar Mahawar <108658753+skm-049@users.noreply.github.com>
2023-12-29 12:25:39 +0530

    Delete server directory

--- server/models/todos.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const TodosSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-});
-const Todos = mongoose.model("Todos", TodosSchema);
-module.exports=Todos;
